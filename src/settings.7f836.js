4e05422efc3a66c3f3ab776fcdb2ad656889ea3e
Ovaltine <562896743@qq.com>
2022-11-27 18:02:21 +0800

    内容更新

--- src/settings.7f836.js
+++
@@ -1,1 +0,0 @@
-window._CCSettings={platform:"web-desktop",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/game.fire",orientation:"",jsList:[],bundleVers:{internal:"d17e2",resources:"7bf22",main:"f39e9"}};
---
+++ src/settings.d2e40.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-desktop",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/game.fire",orientation:"",jsList:[],bundleVers:{internal:"d17e2",resources:"44639",main:"3616c"}};
